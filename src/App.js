6b2c1819d22db4f1eee9f555cecb378b2c60d7aa
Sen Ueda <sen.ueda@icloud.com>
2022-04-03 19:45:42 +0900

    Section 3 Lesson 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
-// Constants
+// Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
 const TWITTER_HANDLE = 'あなたのTwitterハンドル';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  // ユーザーのウォレットアドレスを格納するために使用する状態変数を定義します。
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  // ユーザーがMetaMaskを持っているか確認します。
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+        // accountsにWEBサイトを訪れたユーザーのウォレットアカウントを格納します。
+        // （複数持っている場合も加味、よって account's' と変数を定義している）
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+        // もしアカウントが一つでも存在したら、以下を実行。
+        if (accounts.length !== 0) {
+          // accountという変数にユーザーの1つ目（=Javascriptでいう0番目）のアドレスを格納
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          // currentAccountにユーザーのアカウントアドレスを格納
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // connectWallet メソッドを実装します。
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+      // ウォレットアドレスに対してアクセスをリクエストしています。
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      // ウォレットアドレスを currentAccount に紐付けます。
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // ページがロードされたときに useEffect()内の関数が呼び出されます。
+  useEffect(() => {
+      checkIfWalletIsConnected();
+  }, []);
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
         <p className="header gradient-text">⚡️ METAVERSE GAME ⚡️</p>
-          <p className="sub-text">プレイヤーと協力してボスを倒そう✨</p>
+          <p className="sub-text">プレイヤーと協力してボスを倒そう！</p>
           <div className="connect-wallet-container">
             <img
-              src="https://i.imgur.com/TXBQ4cC.png"
-              alt="LUFFY"
+              src="https://i.imgur.com/yMocj5x.png"
+              alt="Pickachu"
             />
+            {/*
+             * ウォレットコネクトを起動するために使用するボタンを設定しています。
+             * メソッドを呼び出すために onClick イベントを追加することを忘れないでください。
+             */}
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Connect Wallet To Get Started
+            </button>
           </div>
         </div>
         <div className="footer-container">
