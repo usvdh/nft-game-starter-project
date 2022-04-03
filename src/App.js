bb2584926d6c482c5f1bc9d02e7b3dadfbbefa8c
Sen Ueda <sen.ueda@icloud.com>
2022-04-03 22:26:50 +0900

    Section 4 Lesson 1

--- src/App.js
+++ src/App.js
@@ -1,58 +1,139 @@
 import React, { useEffect, useState } from "react";
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
+
+// SelectCharacter に入っているファイルをインポートします。
+import SelectCharacter from "./Components/SelectCharacter";
+import Arena from "./Components/Arena";
+import LoadingIndicator from "./Components/LoadingIndicator";
+
+import { CONTRACT_ADDRESS, transformCharacterData } from "./constants";
+import myEpicGame from "./utils/MyEpicGame.json";
+import { ethers } from "ethers";
 
 // Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
-const TWITTER_HANDLE = 'あなたのTwitterハンドル';
+const TWITTER_HANDLE = "あなたのTwitterハンドル";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-
   // ユーザーのウォレットアドレスを格納するために使用する状態変数を定義します。
   const [currentAccount, setCurrentAccount] = useState(null);
 
-  // ユーザーがMetaMaskを持っているか確認します。
-  const checkIfWalletIsConnected = async () => {
+  // App.js
+  // characterNFT と setCharacterNFT を初期化します。
+  const [characterNFT, setCharacterNFT] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  // ユーザーがRinkeby Network に接続されているか確認します。
+  // '4' は Rinkeby のネットワークコードです。
+  const checkNetwork = async () => {
     try {
-      const { ethereum } = window;
-      if (!ethereum) {
-        console.log('Make sure you have MetaMask!');
-        return;
+      if (window.ethereum.networkVersion !== "4") {
+        alert("Rinkeby Test Network に接続してください!");
       } else {
-        console.log('We have the ethereum object', ethereum);
-        // accountsにWEBサイトを訪れたユーザーのウォレットアカウントを格納します。
-        // （複数持っている場合も加味、よって account's' と変数を定義している）
-        const accounts = await ethereum.request({ method: 'eth_accounts' });
-        // もしアカウントが一つでも存在したら、以下を実行。
-        if (accounts.length !== 0) {
-          // accountという変数にユーザーの1つ目（=Javascriptでいう0番目）のアドレスを格納
-          const account = accounts[0];
-          console.log('Found an authorized account:', account);
-          // currentAccountにユーザーのアカウントアドレスを格納
-          setCurrentAccount(account);
-        } else {
-          console.log('No authorized account found');
-        }
+        console.log("Rinkeby に接続されています.");
       }
     } catch (error) {
       console.log(error);
     }
   };
+
+  // ユーザーがMetaMaskを持っているか確認します。
+  // ユーザーがMetaMaskを持っているか確認します。
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
+
+        // 次の行でreturnを使用するため、ここでisLoadingを設定します。
+        setIsLoading(false);
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+
+        // accountsにWEBサイトを訪れたユーザーのウォレットアカウントを格納します。
+        // （複数持っている場合も加味、よって account's' と変数を定義している）
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+
+        // もしアカウントが一つでも存在したら、以下を実行。
+        if (accounts.length !== 0) {
+          // accountという変数にユーザーの1つ目（=Javascriptでいう0番目）のアドレスを格納
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+
+          // currentAccountにユーザーのアカウントアドレスを格納
+          setCurrentAccount(account);
+        } else {
+          console.log("No authorized account found");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    //すべての関数ロジックの後に、state プロパティを解放します。
+    setIsLoading(false);
+  };
+
+  // レンダリングメソッド
+  // レンダリングメソッド
+  const renderContent = () => {
+    if (isLoading) {
+      return <LoadingIndicator />;
+    }
+
+    // シナリオ1.
+    // ユーザーがWEBアプリにログインしていない場合、WEBアプリ上に、"Connect Wallet to Get Started" ボタンを表示します。
+    if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <img src="https://i.imgur.com/yMocj5x.png" alt="Pickachu" />
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet to Get Started
+          </button>
+        </div>
+      );
+      // シナリオ2.
+      // ユーザーはWEBアプリにログインしており、かつ NFT キャラクターを持っていない場合、WEBアプリ上に、"SelectCharacter Component" を表示します。
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+      // シナリオ3.
+      // ーザーはWEBアプリにログインしており、かつ NFT キャラクターを持っている場合、
+      // Area でボスと戦います。
+    } else if (currentAccount && characterNFT) {
+      return (
+        <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT} />
+      );
+    }
+  };
+
   // connectWallet メソッドを実装します。
   const connectWalletAction = async () => {
     try {
       const { ethereum } = window;
       if (!ethereum) {
-        alert('Get MetaMask!');
+        alert("MetaMask を ダウンロードしてください!");
         return;
       }
+
+      // ユーザーがウォレットを持っているか確認します。
+      checkIfWalletIsConnected();
+
       // ウォレットアドレスに対してアクセスをリクエストしています。
       const accounts = await ethereum.request({
-        method: 'eth_requestAccounts',
+        method: "eth_requestAccounts",
       });
+
       // ウォレットアドレスを currentAccount に紐付けます。
-      console.log('Connected', accounts[0]);
+      console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+
+      // ユーザーが Rinkeby に接続されているか確認します。
+      checkNetwork();
     } catch (error) {
       console.log(error);
     }
@@ -60,30 +141,48 @@ const App = () => {
 
   // ページがロードされたときに useEffect()内の関数が呼び出されます。
   useEffect(() => {
-      checkIfWalletIsConnected();
+    setIsLoading(true);
+    checkIfWalletIsConnected();
   }, []);
+
+  useEffect(() => {
+    // スマートコントラクトを呼び出す関数です。
+    const fetchNFTMetadata = async () => {
+      console.log("Checking for Character NFT on address:", currentAccount);
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicGame.abi,
+        signer
+      );
+
+      const txn = await gameContract.checkIfUserHasNFT();
+      if (txn.name) {
+        console.log("User has character NFT");
+        setCharacterNFT(transformCharacterData(txn));
+      } else {
+        console.log("No character NFT found");
+      }
+    };
+
+    // 接続されたウォレットがある場合のみ、下記を実行します。
+    if (currentAccount) {
+      console.log("CurrentAccount:", currentAccount);
+      fetchNFTMetadata();
+    }
+    setIsLoading(false);
+  }, [currentAccount]);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-        <p className="header gradient-text">⚡️ METAVERSE GAME ⚡️</p>
-          <p className="sub-text">プレイヤーと協力してボスを倒そう！</p>
-          <div className="connect-wallet-container">
-            <img
-              src="https://i.imgur.com/yMocj5x.png"
-              alt="Pickachu"
-            />
-            {/*
-             * ウォレットコネクトを起動するために使用するボタンを設定しています。
-             * メソッドを呼び出すために onClick イベントを追加することを忘れないでください。
-             */}
-            <button
-              className="cta-button connect-wallet-button"
-              onClick={connectWalletAction}
-            >
-              Connect Wallet To Get Started
-            </button>
-          </div>
+          <p className="header gradient-text">⚡️ METAVERSE GAME ⚡️</p>
+          <p className="sub-text">プレイヤーと協力してボスを倒そう✨</p>
+          {/* renderContent メソッドを呼び出します。*/}
+          {renderContent()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
---
+++ src/constants.js
@@ -0,0 +1,15 @@
+const CONTRACT_ADDRESS = '0x558Bc2cbe1DC3E1B14c480492D1Ac4e19303a754';
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
